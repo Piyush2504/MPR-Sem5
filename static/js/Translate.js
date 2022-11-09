e0ec9afb317b152f612334a40a51cb4996fe63ed
Kaustubh Bhirud <kaustubhirud@gmail.com>
2022-11-09 10:58:25 +0530

    Upgrade Design

--- static/js/Translate.js
+++ static/js/Translate.js
@@ -51,7 +51,7 @@ if (navigator.mediaDevices.getUserMedia) {
 			mediaRecorder.start();
 			console.log(mediaRecorder.state);
 			console.log("recorder started");
-			record.style.background = "red";
+			// record.style.background = "red";
 
 			stop.disabled = false;
 			record.disabled = true;
@@ -72,50 +72,50 @@ if (navigator.mediaDevices.getUserMedia) {
 		mediaRecorder.onstop = function (e) {
 			console.log("data available after MediaRecorder.stop() called.");
 
-			const clipName = "My unnamed clip" || prompt("Enter a name for your sound clip?", "My unnamed clip");
+			// const clipName = "My unnamed clip" || prompt("Enter a name for your sound clip?", "My unnamed clip");
 
-			const clipContainer = document.createElement("article");
-			const clipLabel = document.createElement("p");
-			const audio = document.createElement("audio");
-			const deleteButton = document.createElement("button");
+			// const clipContainer = document.createElement("article");
+			// const clipLabel = document.createElement("p");
+			// const audio = document.createElement("audio");
+			// const deleteButton = document.createElement("button");
 
-			clipContainer.classList.add("clip");
-			audio.setAttribute("controls", "");
-			deleteButton.textContent = "Delete";
-			deleteButton.className = "delete";
+			// clipContainer.classList.add("clip");
+			// audio.setAttribute("controls", "");
+			// deleteButton.textContent = "Delete";
+			// deleteButton.className = "delete";
 
-			if (clipName === null) {
-				clipLabel.textContent = "My unnamed clip";
-			} else {
-				clipLabel.textContent = clipName;
-			}
+			// if (clipName === null) {
+			// 	clipLabel.textContent = "My unnamed clip";
+			// } else {
+			// 	clipLabel.textContent = clipName;
+			// }
 
-			clipContainer.appendChild(audio);
-			clipContainer.appendChild(clipLabel);
-			clipContainer.appendChild(deleteButton);
-			soundClips.appendChild(clipContainer);
+			// clipContainer.appendChild(audio);
+			// clipContainer.appendChild(clipLabel);
+			// clipContainer.appendChild(deleteButton);
+			// soundClips.appendChild(clipContainer);
 
-			audio.controls = true;
+			// audio.controls = true;
 			const blob = new Blob(chunks, { type: "audio/ogg; codecs=opus" });
 			transcript(blob);
 			chunks = [];
-			const audioURL = window.URL.createObjectURL(blob);
-			audio.src = audioURL;
+			// const audioURL = window.URL.createObjectURL(blob);
+			// audio.src = audioURL;
 			console.log("recorder stopped");
 
-			deleteButton.onclick = function (e) {
-				e.target.closest(".clip").remove();
-			};
+			// deleteButton.onclick = function (e) {
+			// 	e.target.closest(".clip").remove();
+			// };
 
-			clipLabel.onclick = function () {
-				const existingName = clipLabel.textContent;
-				const newClipName = prompt("Enter a new name for your sound clip?");
-				if (newClipName === null) {
-					clipLabel.textContent = existingName;
-				} else {
-					clipLabel.textContent = newClipName;
-				}
-			};
+			// clipLabel.onclick = function () {
+			// 	const existingName = clipLabel.textContent;
+			// 	const newClipName = prompt("Enter a new name for your sound clip?");
+			// 	if (newClipName === null) {
+			// 		clipLabel.textContent = existingName;
+			// 	} else {
+			// 		clipLabel.textContent = newClipName;
+			// 	}
+			// };
 		};
 
 		mediaRecorder.ondataavailable = function (e) {
